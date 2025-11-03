11282e186f47b0f7d6cb94c2fa0d71ba49e2b684
Joanna Kliś <joannnaklis8@gmail.com>
2025-11-03 14:35:46 +0100

    added styles for dashboard

--- Routing.php
+++ Routing.php
@@ -2,6 +2,7 @@
 
 require_once 'src/controllers/SecurityController.php';
 require_once 'src/controllers/UserController.php';
+require_once 'src/controllers/DashboardController.php';
 
 class Routing {
 
@@ -15,6 +16,10 @@ class Routing {
         "register" => [
             "controller" => "SecurityController",
             "action" => "register"
+        ],
+        "dashboard" => [
+            "controller" => "DashboardController",
+            "action" => "index"
         ]
     ];
 
@@ -23,15 +28,28 @@ class Routing {
             return self::$controllerInstances[$controllerClass];
         }
         
+        // Klasa kontrolera jest ładowana przez require_once na początku
         $instance = new $controllerClass();
         self::$controllerInstances[$controllerClass] = $instance;
 
         return $instance;
     }
 
+
     public static function run(string $path) {
-        $user_details_regex = '/^user\/(\d+)$/';
+        // /dashboard/12234 -- wyciagnie nam jakis element o wskazanym ID
+        $dashboard_details_regex = '/^dashboard(?:\/(\d+))?$/'; // ścieżka do "dashboard" z opcjonalnym ID
+        $user_details_regex = '/^user\/(\d+)$/'; // ścieżka do "user{id}"
         
+        if (preg_match($dashboard_details_regex, $path, $matches)) {
+            // $matches[1] będzie zawierać przechwycone ID lub będzie puste/null
+            $id = $matches[1] ?? null;
+        
+            $controllerObj = self::getControllerInstance("DashboardController");
+            $controllerObj->index($id); // wywołanie metody z ID lub null
+            return;
+        }
+
         if (preg_match($user_details_regex, $path, $matches)) {
             // $matches[1] będzie zawierać przechwycone ID użytkownika (np. 4578)
             $userId = $matches[1];
@@ -41,24 +59,17 @@ class Routing {
             return;
         }
 
-        switch($path){
-            case 'dashboard':
-                // TODO: connect with database
-                // get elements to present on dashboard
 
-                include 'public/views/dashboard.html';
-                break;
-            case 'login':
-            case 'register':
-                $controller = Routing::$routes[$path]["controller"];
-                $action = Routing::$routes[$path]["action"];
+        // obsługa ścieżek
+        if (isset(self::$routes[$path])) {
+            $controller = self::$routes[$path]["controller"];
+            $action = self::$routes[$path]["action"];
 
-                $controllerObj = self::getControllerInstance($controller);
-                $controllerObj->$action(); 
-                break;
-            default:
-                include 'public/views/404.html';
-                break;
+            $controllerObj = self::getControllerInstance($controller);
+            $controllerObj->$action(); 
+            return;
         }
+
+        include 'public/views/404.html';
     }
 }
