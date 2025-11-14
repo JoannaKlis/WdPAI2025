ddaa3b2bb617b3ebe749d2ef09e2f4e458b39047
Joanna Kliś <joannnaklis8@gmail.com>
2025-11-14 16:47:58 +0100

    adding registration and connecting the transition with login

---
+++ public/scripts/registration.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const passwordField = document.getElementById('password-field');
+    const togglePassword = document.getElementById('toggle-password');
+    
+    const confirmPasswordField = document.getElementById('confirm-password-field');
+    const toggleConfirmPassword = document.getElementById('toggle-confirm-password');
+
+    // przełączanie widoczności hasła
+    function toggleVisibility(field, toggle) {
+        if (field && toggle) {
+            toggle.addEventListener('click', function () {
+                // przełączenie typu pola z 'password' na 'text' i odwrotnie
+                const type = field.getAttribute('type') === 'password' ? 'text' : 'password';
+                field.setAttribute('type', type);
+                
+                if (type === 'text') {
+                    toggle.setAttribute('src', 'public/img/visible.png'); 
+                } else {
+                    toggle.setAttribute('src', 'public/img/invisible.png');
+                }
+            });
+        }
+    }
+
+    // obsługa pierwszego pola hasła
+    toggleVisibility(passwordField, togglePassword);
+    
+    // obsługa pola potwierdzenia hasła
+    toggleVisibility(confirmPasswordField, toggleConfirmPassword);
+});
--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -23,4 +23,30 @@ class SecurityController extends AppController {
         // tymczasowy komunikat dopóki nie ma bazy:
         return $this->render("login", ["message" => "Incorrect email or password!"]);
     }
+
+
+    public function register() {
+        if (!$this->isPost()) {
+            return $this->render("registration");
+        }
+
+        // pobranie danych do rejestracji
+        $name = $_POST['name'] ?? null;
+        $email = $_POST['email'] ?? null;
+        $password = $_POST['password'] ?? null;
+        $confirmedPassword = $_POST['confirmedPassword'] ?? null;
+
+        // walidacja pustych pól (wszystkie muszą być wypełnione)
+        if (empty($name) || empty($email) || empty($password) || empty($confirmedPassword)) {
+            return $this->render("registration", ["message" => "Enter data designation in all fields!"]);
+        }
+        
+        // walidacja zgodności haseł
+        if ($password !== $confirmedPassword) {
+            return $this->render("registration", ["message" => "The passwords are not identical!"]);
+        }
+
+        // tymczasowy komunikat dopóki nie ma bazy:
+        return $this->render("registration", ["message" => "Your account has been created!"]);
+    }
 }
