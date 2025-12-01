189f6ae3f818b624b2ed276029611758d0bee37c
Joanna Kliś <joannnaklis8@gmail.com>
2025-12-01 20:49:16 +0100

    improving data validation

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -46,61 +46,43 @@ class SecurityController extends AppController {
         $password = $_POST['password'] ?? '';
         $confirmedPassword = $_POST['confirmedPassword'] ?? '';
 
-        // // walidacja czy wszystkie pola są wypełnione
-        // if (empty($firstname) || empty($lastname) || empty($email) || empty($password) || empty($confirmedPassword)) {
-        //     return $this->render('registration', ['messages' => 'All fields are required!']);
-        // }
+        // TODO: dodać walidację danych na first i last name aby nie zawierały niepożądanych znaków
 
-        // // ^[a-zA-Z]+$ - tylko małe i duże litery dla imienia i nazwiska
-        // if (!preg_match('/^[a-zA-Z]+$/', $firstname) || !preg_match('/^[a-zA-Z]+$/', $lastname)) {
-        //     return $this->render('registration', ['messages' => 'First name and last name can only contain letters.']);
-        // }
+        // walidacja formatu email (musi zawierać @ i domenę po .)
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return $this->render('registration', ['messages' => 'Email address is incorrect!']);
+        }
 
-        // // normalizacja imienia i nazwiska
-        // $firstname = ucfirst(strtolower($firstname));
-        // $lastname = ucfirst(strtolower($lastname));
+        // walidacja zgodności haseł
+        if ($password !== $confirmedPassword) {
+            return $this->render('registration', ['messages' => 'Passwords should be the same!']);
+        }
 
-        // // walidacja formatu email (musi zawierać @ i domenę po .)
-        // if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        //     return $this->render('registration', ['messages' => 'Email address is incorrect!']);
-        // }
+        // funkcja pomocnicza do walidacji hasła
+        $isValidPassword = function (string $password): bool {
+            if (strlen($password) < 6) return false; // min. 6 znaków
+            if (!preg_match('/[A-Z]/', $password)) return false; // min. 1 duża litera
+            if (!preg_match('/[0-9]/', $password)) return false; // min. 1 cyfra
+            if (!preg_match('/[^A-Za-z0-9]/', $password)) return false; // min. 1 znak specjalny
+            return true;
+        };
 
-        // // walidacja unikalności e-maila
-        // if ($this->userRepository->getUserByEmail($email)) {
-        //     return $this->render('registration', ['messages' => 'User with this email already exists!']);
-        // }
+        // walidacja siły hasła i unikalności emaila (do poprawy w przyszłości)
+        if (!$isValidPassword($password || $this->userRepository->getUserByEmail($email))) {
+            return $this->render('registration', ['messages' => 'Inccorect email or
+            Password must be at least 6 characters long, 1 uppercase letter, 1 number and 1 special character.']);
+        }   
 
-        // // walidacja zgodności haseł
-        // if ($password !== $confirmedPassword) {
-        //     return $this->render('registration', ['messages' => 'Passwords should be the same!']);
-        // }
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 
-        // // funkcja pomocnicza do walidacji hasła
-        // $isValidPassword = function (string $password): bool {
-        //     if (strlen($password) < 6) return false; // min. 6 znaków
-        //     if (!preg_match('/[A-Z]/', $password)) return false; // min. 1 duża litera
-        //     if (!preg_match('/[0-9]/', $password)) return false; // min. 1 cyfra
-        //     if (!preg_match('/[^A-Za-z0-9]/', $password)) return false; // min. 1 znak specjalny
-        //     return true;
-        // };
-
-        // // walidacja siły hasła
-        // if (!$isValidPassword($password)) {
-        //     return $this->render('registration', ['messages' => 'Password must be at least 6 characters long, 1 uppercase letter, 1 number and 1 special character.']);
-        // }
-
-        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-
-        // dodanie użytkownika do bazy
-        $this->userRepository->createUser(
-            $firstname,
-            $lastname,
-            $email,
-            $hashedPassword
-        );
-
+    // dodanie użytkownika do bazy
+    $this->userRepository->createUser(
+        $firstname,
+        $lastname,
+        $email,
+        $hashedPassword
+    );
         $url = "http://$_SERVER[HTTP_HOST]";
         header("Location: {$url}/login?registered=true");
-        exit();
     }
 }
