43888fb0096496b54065167812f9a9d4b2900ae5
Joanna Kliś <joannnaklis8@gmail.com>
2026-01-06 20:53:02 +0100

    refactor add care modals to inline modal dialogs

--- public/scripts/deleteAddWindow.js
+++ public/scripts/deleteAddWindow.js
@@ -14,10 +14,24 @@ function closeDeleteModal() {
     document.getElementById('deleteModal').style.display = 'none';
 }
 
+// ekran dodawania
+function openAddModal() {
+    document.getElementById('addModal').style.display = 'flex';
+}
+
+function closeAddModal() {
+    document.getElementById('addModal').style.display = 'none';
+}
+
 // zamknij modal, jeśli kliknięto w tło
 window.onclick = function(event) {
-    const modal = document.getElementById('deleteModal');
-    if (event.target == modal) {
-        modal.style.display = "none";
+    const deleteModal = document.getElementById('deleteModal');
+    const addModal = document.getElementById('addModal');
+    
+    if (event.target == deleteModal) {
+        deleteModal.style.display = "none";
+    }
+    if (event.target == addModal) {
+        addModal.style.display = "none";
     }
 }
