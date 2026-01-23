2e86b6e5fe5b7c22200048503cd40aff39fe6bfb
Joanna Kliś <joannnaklis8@gmail.com>
2026-01-23 22:38:11 +0100

    improved appearance of registration view

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    firstname VARCHAR(100) NOT NULL,
-    lastname VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    firstname VARCHAR(150) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(200) NOT NULL,
     role VARCHAR(20) DEFAULT 'user',
     picture_url TEXT,
     enabled BOOLEAN DEFAULT TRUE,
--- public/views/partials/header.php
+++ public/views/partials/header.php
@@ -3,5 +3,5 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type= "text/css" href="https://fonts.googleapis.com/css2?family=Jua&display=swap">
-    <link rel="stylesheet" type= "text/css" href="public/styles/common.css">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Jua&display=swap">
+    <link rel="stylesheet" type="text/css" href="public/styles/common.css">
