6a471fa7382b872691788a6923704d50a687bc94
Joanna Kliś <161654208+JoannaKlis@users.noreply.github.com>
2026-09-20 16:02:58 +0200

    docker update

--- src/Database.php
+++ src/Database.php
@@ -8,17 +8,18 @@ class Database {
     private $password;
     private $host;
     private $database;
-    
+
     private static $instance = null;
-    private $conn; 
-    
+    private $conn;
+
     private function __construct()
     {
-        $this->username = getenv("DB_USER");
-        $this->password = getenv("DB_PASSWORD");
-        $this->host = getenv("DB_HOST");
-        $this->database = getenv("DB_NAME");
+        $this->username = getenv('DB_USER') ?: USERNAME;
+        $this->password = getenv('DB_PASSWORD') ?: PASSWORD;
+        $this->host = getenv('DB_HOST') ?: HOST;
+        $this->database = getenv('DB_NAME') ?: DATABASE;
     }
+
     public static function getInstance(): Database
     {
         if (self::$instance === null) {
@@ -30,20 +31,20 @@ class Database {
     public function connect()
     {
         try {
-            $port = getenv("DB_PORT");
+            $port = getenv('DB_PORT') ?: '5432';
 
             $this->conn = new PDO(
-                "pgsql:host=$this->host;port=$port;dbname=$this->database",
+                "pgsql:host={$this->host};port={$port};dbname={$this->database}",
                 $this->username,
-                $this->password,
-                ["sslmode" => "require"]
+                $this->password
             );
 
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $this->conn;
         }
-        catch(PDOException $e) {
-            include 'public/views/errors/500.html';
+        catch (PDOException $e) {
+            http_response_code(500);
+            include __DIR__ . '/../public/views/errors/500.html';
             exit();
         }
     }
