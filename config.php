76b898ae136caed6fbbc921b023805dc14b6f45d
Joanna Kliś <161654208+JoannaKlis@users.noreply.github.com>
2026-09-20 15:39:30 +0200

    docker update 2

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
 
-$USERNAME = getenv("DB_USER");
-$PASSWORD = getenv("DB_PASSWORD");
-$HOST = getenv("DB_HOST");
-$DATABASE = getenv("DB_NAME");
-$PORT = getenv("DB_PORT");
+const USERNAME = "docker";
+const PASSWORD = "docker";
+const HOST = "db";
+const DATABASE = "db";
